2617a89b59642ad008d3392a954da6cd7926a689
sushmit <sushmitsarmah@gmail.com>
2022-12-04 05:41:49 +0530

    contracts updated

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,36 @@
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygon_mainnet: {
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygon_mumbai: process.env.MUMBAISCAN_API_KEY,
+      polygon_mainnet: process.env.POLYGONSCAN_API_KEY
+    }
+  },  
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -7,20 +7,44 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await hre.ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // console.log(
+  //   `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // );
+
+  const TimeLock = await hre.ethers.getContractFactory("TimeLock");
+  const timelock = await TimeLock.deploy(20, [], []);
+  await timelock.deployed();
+
+  console.log(`Time Lock deployed to ${timelock.address}`);
+
+  const PlayAstraToken = await hre.ethers.getContractFactory("PlayAstraToken");
+  const token = await PlayAstraToken.deploy();
+  await token.deployed();
+
+  console.log(`Play Astra Token deployed to ${token.address}`);
+
+  const PlayAstraGovernor = await hre.ethers.getContractFactory("PlayAstraGovernor");
+  const governor = await PlayAstraGovernor.deploy(token.address, timelock.address);
+  await governor.deployed();
+
+  console.log(`Play Astra Governor deployed to ${governor.address}`);
+
+  const PlayAstraNFT = await hre.ethers.getContractFactory("PlayAstraNFT");
+  const nft = await PlayAstraNFT.deploy();
+  await nft.deployed();
+
+  console.log(`Play Astra NFT deployed to ${nft.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
